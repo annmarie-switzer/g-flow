77e47b6fc0cb2de3a5ff6363e8d14c9fb0b14cbe
Annmarie Switzer <aargiros@gmail.com>
2024-06-09 14:55:46 -0400

    popup should fetch threads, not token

--- src/popup.js
+++ src/popup.js
@@ -1,14 +1,7 @@
 import { generateThreadList } from './threads/threads.js';
 import { generateTimeline } from './timeline/timeline.js';
 
-chrome.runtime.sendMessage({ action: 'getAccessToken' }, (token) => {
-  if (token) {
-    generateThreadList();
-    generateTimeline('today');
-  } else {
-    console.error(
-      "Popup couldn't get access token: ",
-      chrome.runtime.lastError
-    );
-  }
+chrome.runtime.sendMessage({ action: 'fetchUnreadThreads' }, () => {
+  generateThreadList();
+  generateTimeline('today');
 });
